f860b1c1dba7b3e9224d2df5a8b776c0db014df5
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2022-06-21 23:50:14 +0900

    Initial Commit

---
+++ AppStoreClone/AppStoreCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppStoreCloneApp.swift
+//  AppStoreClone
+//
+//  Created by 이창형 on 2022/06/21.
+//
+
+import SwiftUI
+
+@main
+struct AppStoreCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
